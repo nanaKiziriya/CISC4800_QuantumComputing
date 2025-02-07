741107542ea78ee50056ce2e0e98a8acf84d0e9d
nanaKiziriya <144845521+nanaKiziriya@users.noreply.github.com>
2025-02-06 22:11:38 -0500

    Create ComplexNumConverter.java

---
+++ Chapter02/Program 1.03/ComplexNumConverter.java
@@ -0,0 +1,22 @@
+public class ComplexNumConverter{
+  // convert between general (cartesian), polar, and exponential (Euler's formula) form for complex numbers
+
+  public static void main(String[]args){
+    //driver
+  }
+
+  class GenC{
+
+  }
+
+  class PolC{
+
+  }
+
+  class ExpC extends PolC{
+    public String toString(){
+      return //;
+    }
+  }
+
+}
