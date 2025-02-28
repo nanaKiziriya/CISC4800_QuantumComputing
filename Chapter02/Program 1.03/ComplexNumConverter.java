5d4f390bf610660883559442395859917a536546
nanaKiziriya <144845521+nanaKiziriya@users.noreply.github.com>
2025-02-27 22:36:21 -0500

    Update ComplexNumConverter.java

--- Chapter02/Program 1.03/ComplexNumConverter.java
+++ Chapter02/Program 1.03/ComplexNumConverter.java
@@ -1,28 +1,19 @@
 public class ComplexNumConverter{
     // convert between general (cartesian), polar, and exponential (Euler's formula) form for complex numbers
-
     public static void main(String[]args){
         //driver
+        System.out.println((Math.PI*2)/Math.PI);
     }
 }
 
-public class ComplexNumber{
-    int radius;
-    int theta
-    
-}
-
-absract class ComplexNumber{
-    public String toString();
-}
+absract class ComplexNumber{}
 
 class GenComplex extends ComplexNumber{
     double real, imaginary;
     public String toString(){
-
+        
     }
 }
-    
 
 class PlrComplex extends ComplexNumber{
     double radius, piRadians;
@@ -34,6 +25,6 @@ class PlrComplex extends ComplexNumber{
 class ExpComplex extends PlrComplex{
     // r*exp(theta*i)
     public String toString(){
-        return 
+        return String.format("",radius,piRadians);
     }
 }
